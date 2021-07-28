6804f5b771cfff995033d336ca25e14230c645bc
firmanarrs <62589436+firman720@users.noreply.github.com>
2021-07-29 00:31:45 +0700

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -28,6 +28,34 @@
 		}
 	}
 
+	if(isset($_POST['masal'])){
+
+		$datanama  =  $_FILES['data']['name'];
+		$datatmp   =  $_FILES['data']['tmp_name'];	
+		$exe       =  pathinfo($datanama, PATHINFO_EXTENSION);
+		$folder    = 'file/';
+         
+		if($exe=='csv'){
+			$upload = move_uploaded_file($datatmp, $folder.$datanama);
+			if($upload){
+				$open = fopen($folder.$datanama, 'r');
+				while (($row = fgetcsv($open, 1000, ','))!==FALSE ) {
+					    $sql = mysql_query("INSERT INTO siswa 
+					    VALUES('','".$row[0]."','".$row[1]."','".$row[2]."') ")or die(mysql_error());
+
+				}
+				header('location: index.php');
+			}else{
+				echo "<script>alert('Gagal diupload')</script>";
+				header('location: index.php');
+			}
+		}else{
+			echo "<script>alert('Bukan File CSV')</script>";
+			header('location: index.php');
+		}
+
+	}
+
 	if($_GET['act']=='hapus'){
 		$id = $_GET['id'];
 
